9042a6b8af893b9600a07a6d53dbb7c37a7b472f
zzl <86543402@qq.com>
2021-06-21 13:44:16 +0800

    refactor: 使用路径匹配

--- server/src/main/kotlin/com/potato/cloud/controller/FileController.kt
+++ server/src/main/kotlin/com/potato/cloud/controller/FileController.kt
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ZeroCopyHttpOutputMessage
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.ui.Model
 import org.springframework.util.FileSystemUtils
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import java.io.File
 import java.io.IOException
-import java.net.URLDecoder
 import java.util.*
 import kotlin.io.path.exists
 
@@ -29,75 +27,65 @@ class FileController(config: ConfigProperties) {
     private val BASE_PATH = config.filePath
 
     @PassAuth
-    @GetMapping("/files/**")
+    @GetMapping("/files/{*path}")
     fun getFiles(
         model: Model,
-        request: ServerHttpRequest,
+        @PathVariable("path", required = false) inputPath: String?
     ): Mono<ResponseWrap<MutableList<FileInfo>>> {
-        return Mono.create<String> {
-            val requestPath = request.path.value()
-            it.success(URLDecoder.decode(requestPath, "utf-8").removePrefix("/files").removePrefix("/"))
-        }.flatMap { path ->
-            val resp: ResponseWrap<MutableList<FileInfo>> = ResponseWrap()
-            val realPath = if (path.isBlank()) BASE_PATH else BASE_PATH + File.separator + path
-            val dir = File(realPath)
-            if (!dir.exists() || !dir.isDirectory) {
-                resp.status = 1
-                resp.msg = "参数错误!"
-                return@flatMap Mono.just(resp)
-            }
-            val rootPath = dir.invariantSeparatorsPath.removePrefix(BASE_PATH.replace("\\", "/")).removePrefix("/")
-
-            val fileList: MutableList<FileInfo> = LinkedList()
-            dir.list()?.let { list ->
-                for (name in list) {
-                    val f = File(dir, name)
-                    val relativePath = if (rootPath.isBlank()) name else "$rootPath/$name"
-                    fileList.add(
-                        FileInfo(
-                            name,
-                            if (f.isDirectory) FileType.DIR else FileType.FILE,
-                            f.lastModified() / 1000,
-                            f.length(),
-                            relativePath,
-                            (if (f.isFile) "/download/" else "/files/") + relativePath
-                        )
-                    )
-                }
-                resp.status = 0
-                resp.data = fileList
-            }
-            return@flatMap Mono.just(resp)
+        val path: String = inputPath.orEmpty()
+        val resp: ResponseWrap<MutableList<FileInfo>> = ResponseWrap()
+        val realPath = if (path.isBlank()) BASE_PATH else BASE_PATH + File.separator + path
+        val dir = File(realPath)
+        if (!dir.exists() || !dir.isDirectory) {
+            resp.status = 1
+            resp.msg = "参数错误!"
+            return Mono.just(resp)
         }
+        val rootPath = dir.invariantSeparatorsPath.removePrefix(BASE_PATH.replace("\\", "/")).removePrefix("/")
+
+        val fileList: MutableList<FileInfo> = LinkedList()
+        dir.list()?.let { list ->
+            for (name in list) {
+                val f = File(dir, name)
+                val relativePath = if (rootPath.isBlank()) name else "$rootPath/$name"
+                fileList.add(
+                    FileInfo(
+                        name,
+                        if (f.isDirectory) FileType.DIR else FileType.FILE,
+                        f.lastModified() / 1000,
+                        f.length(),
+                        relativePath,
+                        (if (f.isFile) "/download/" else "/files/") + relativePath
+                    )
+                )
+            }
+            resp.status = 0
+            resp.data = fileList
+        }
+        return Mono.just(resp)
+
 
     }
 
     @PassAuth
-    @GetMapping("/download/**")
-    fun download(request: ServerHttpRequest, response: ServerHttpResponse): Mono<Void> {
-        return Mono.create<String> {
-            it.success(
-                URLDecoder.decode(request.path.value(), "utf-8")
-                    .removePrefix("/download")
-                    .removePrefix("/")
-            )
-        }.flatMap { path ->
-            try {
-
-                val file = File("$BASE_PATH/$path")
-                if (!file.exists()) {
-                    response.rawStatusCode = 404
-                    logger.info(file.absolutePath + " 不存在")
-                    return@flatMap response.writeAndFlushWith(Mono.empty())
-                }
-                val zeroCopyResponse = response as ZeroCopyHttpOutputMessage
-                response.getHeaders().contentLength = file.length()
-                return@flatMap zeroCopyResponse.writeWith(file, 0, file.length())
-            } catch (e: IOException) {
-                logger.error(e.message, e)
+    @GetMapping("/download/{*path}")
+    fun download(@PathVariable("path", required = false) inputPath: String?, response: ServerHttpResponse): Mono<Void> {
+        val path: String = inputPath.orEmpty()
+        try {
+            val file = File("$BASE_PATH/$path")
+            if (!file.exists()) {
+                response.rawStatusCode = 404
+                logger.info(file.absolutePath + " 不存在")
+                return response.writeAndFlushWith(Mono.empty())
             }
-            return@flatMap response.writeAndFlushWith(Mono.empty())
+            val zeroCopyResponse = response as ZeroCopyHttpOutputMessage
+            response.getHeaders().contentLength = file.length()
+            return zeroCopyResponse.writeWith(file, 0, file.length())
+        } catch (e: IOException) {
+            logger.error(e.message, e)
         }
+        return response.writeAndFlushWith(Mono.empty())
+
 
     }
 
